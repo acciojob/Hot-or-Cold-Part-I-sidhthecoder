4d73183eded4a8279aa79822b4f4150d4aeeba52
Siddhant Maurya <88020811+sidhthecoder@users.noreply.github.com>
2023-02-23 22:37:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,22 +1,39 @@
-var btn = document.getElementById("btn");
-var num = document.getElementById("num");
-var res=  document.getElementById("respond");
-var guess= document.getElementById("guess").value;
-function randomNumGenerator() {
-  randomNum = Math.floor(Math.random() * 41) - 20;
-  document.getElementById("num").innerText = randomNum;
-  guessNum();
-  print();
-}
+// generate a random number between 1 and 100
+const secretNumber = Math.floor(Math.random() * 100) + 1;
 
-function guessNum() {
-  guess = Number(document.getElementById("guess").value);
-}
-function print() {
-  if (Math.abs(randomNum - guess) <= 5) {
-    document.getElementById("respond").innerText = "Hot";
-  } else {
-    document.getElementById("respond").innerHTML = "Cold";
+// initialize previous guess and difference
+let previousGuess = 0;
+let previousDifference = 0;
+
+function checkGuess() {
+  // get user's guess
+  const guess = Number(document.getElementById("guess").value);
+
+  // check if the guess is correct
+  if (guess === secretNumber) {
+    document.getElementById("response").textContent = "Congratulations, you guessed the secret number!";
+    return;
   }
+
+  // check if this is the first guess
+  if (previousGuess === 0) {
+    previousGuess = guess;
+    previousDifference = Math.abs(secretNumber - guess);
+    document.getElementById("response").textContent = guess < secretNumber ? "Guess higher" : "Guess lower";
+    return;
+  }
+
+  // calculate the difference between the current guess and the secret number
+  const difference = Math.abs(secretNumber - guess);
+
+  // compare with the previous difference and provide feedback
+  if (difference < previousDifference) {
+    document.getElementById("response").textContent = "Getting hotter! Guess " + (guess < previousGuess ? "higher" : "lower");
+  } else {
+    document.getElementById("response").textContent = "Getting colder! Guess " + (guess < secretNumber ? "higher" : "lower");
+  }
+
+  // update the previous guess and difference
+  previousGuess = guess;
+  previousDifference = difference;
 }
-btn.addEventListener("click",randomNumGenerator);
